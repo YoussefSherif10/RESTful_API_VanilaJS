31fa859692a294db0104e77de5b94d28d0218f8c
yousseffekry <mr-youssefsherif@hotmail.com>
2023-03-22 18:52:21 +0200

    Implemented the getRequestData function to return the body in string form

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,19 @@
 const getRequestData = (req) => {
- // Write logic here to read the request body data
+    return new Promise((resolve, reject) => {
+        try {
+            let body = '';
+
+            req.on('data', (data) => {
+                body += data.toString();
+            })
+
+            req.on('end', () => {
+                resolve(body);
+            })
+        } catch(e) {
+            reject(e);
+        }
+    });
 }
 
 module.exports = getRequestData
